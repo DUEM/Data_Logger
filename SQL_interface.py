fc9a2cf708e094525deef77bd4101a4d0f99adc2
usmaanjaved <usm44njaved@gmail.com>
2014-11-22 20:21:36 +0000

    Update SQL_interface.py

--- SQL_interface.py
+++ SQL_interface.py
@@ -1,1 +1,10 @@
 import mysql.connector as sql
+
+connect = sql.connect(user = "python1", database = "test") # connected to database
+cursor = connect.cursor()
+add_message = ("INSERT INTO can " 
+               "( Time, Node ID, Node Type, Data )"
+               "VALUES (%s,%s,%s,%s) ") 
+message_data = ()#place data in here
+cursor.execute(add_message,message_data) # updating the table
+#this code doesnt work and im not sure why yet
