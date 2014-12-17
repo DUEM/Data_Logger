3189c5162bdf3b2082b9a9bfa2bafb1d4d63afcf
usmaanjaved <usm44njaved@gmail.com>
2014-12-17 19:55:05 +0000

    Added query code
    need to put them in actual functions. as well as some sort of UI to change the values

--- SQL_interface.py
+++ SQL_interface.py
@@ -6,3 +6,14 @@ add_message = ("INSERT INTO can ( `Time`, `Node ID`, `Node Type`, `Data` )VALUES
 cursor.execute(add_message) # updating the table
 connect.commit()
 #this code doesnt work and im not sure why yet
+
+######################### querying a table ####################################################
+query = ("SELECT `Node ID`,`Node Type`, `Data` FROM can WHERE `Node ID` BETWEEN 1 AND 3")
+# choose which columns you want outputted          "SELECT `Node ID`,`Node Type`, `Data` FROM can"
+# choose how you wish to filter with               "WHERE `Node ID` BETWEEN 1 AND 3"
+cursor.execute(query)
+
+for (item) in cursor:
+  print(item) # v.basic printing
+    
+#Need to make the values within the command editable (use string conc.)
