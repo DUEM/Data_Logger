f065b9a40d7ed33832838f5db8019b26e1ce37e0
usmaanjaved <usm44njaved@gmail.com>
2014-12-22 19:34:00 +0000

    added SQL stuff not sure if it works

--- CANUSB_Interface.py
+++ CANUSB_Interface.py
@@ -1,5 +1,38 @@
 
 import serial
+import mysql.connector as sql
+import time   
+
+#################################### SQL FUNCTIONS WE WILL NEED #########################################################
+def connect(username,database):
+    connect = sql.connect(user = username, database = database) # connected to database
+    return connect
+def cursor(connect):
+    cursor = connect.cursor()
+    return cursor
+def add_message(node_id,data,cursor,connect):
+    time1 = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    ###################################### Creating the Argument #######################
+    add_message = "INSERT INTO can ( `Time`, `Node ID`, `Data` )VALUES ('"
+    add_message += time1
+    add_message += "','"
+    add_message += node_id
+    add_message += "','"
+    add_message += data
+    add_message += "')"
+
+    cursor.execute(add_message) 
+    connect.commit()
+    return 1
+def query(cursor,connect):
+    arg = ("SELECT `Node ID`,`Time`, `Data` FROM can WHERE `Node ID` BETWEEN 0 AND 1000")
+    # make query editable
+    cursor.execute(arg)
+    return cursor
+#########################################################################################################################
+
+
 #open port
 ser = serial.Serial()
 ser.port = "COM7"
@@ -30,6 +63,15 @@ while t<100: #arbitrary number to ensure port eventually closes
         data=[]
         for j in range(5,21,2):
             data.append(int("0x"+y[j]+y[j+1],0))
+            # can data be a stored as a string as opposed to an array? 
+            
+    ######### SQL BIT HERE ########################################
+    connect = connect("InsertUserNameHere","InsertTableNameHere")
+    cursor = cursor(connect)
+    add_message(str(node_id),str(data),cursor,connect)
+    ###############################################################
+    
+    
         if node_id == 1:
             print("this was node 1")
             print data
