a3a363d415de2255d5e002c2c04148d687a17c4c
edwardsainsbury <edwardsainsbury@me.com>
2014-12-23 19:16:47 +0000

    Message sending

--- CANUSB_Interface.py
+++ CANUSB_Interface.py
@@ -1,57 +1,73 @@
-
+#import SQL_Interface
 import serial
+import io
 #open port
-ser = serial.Serial()
-ser.port = "COM7"
-ser.baudrate = 115200
-ser.timeout = 1
-ser.open()
-if(ser.isOpen()):
-    print("Port opened")
-    ser.write(b"\r\r\r") #Clear residual messages 
-    ser.write(b"S6\r") #Set canusb speed of 500kbit/s
-    ser.write(b"O\r") #Open canusb
-    print(ser.read(5))
-    print("CANUSB activated")
-
-else:
-    print( "Port did not open")
-
-#Sort messages
-t=0
-while t<100: #arbitrary number to ensure port eventually closes
-    print("No. " +str(t))
-    x= ser.read(22)
-    print(x)
-    y=list(x)
-    if x != "":
-        joined = "0x"+y[1]+y[2]+y[3]
-        node_id = int(joined,0)
-        data=[]
-        for j in range(5,21,2):
-            data.append(int("0x"+y[j]+y[j+1],0))
-        if node_id == 1:
-            print("this was node 1")
-            print data
-        elif node_id == 2:
-            print("something else")
-        elif node_id == 3:
-            print("something else")
-        elif node_id == 4:
-            print("something else")
-        elif node_id == 5:
-            print("something else")
-        elif node_id == 6:
-            print("something else")
-        elif node_id == 7:
-            print("something else")
-        else:
-            print("this was node 0")
-            print data
-        t+=1
+def open_ports(port,baudrate,timeout):
+    ser = serial.Serial()
+    ser.port = port
+    ser.baudrate = baudrate
+    ser.timeout = timeout
+    ser.open()
+    if(ser.isOpen()):
+        print("Port opened")
+        ser.write(b"\r\r\r") #Clear residual messages
+        ser.write(b"S6\r") #Set canusb speed of 500kbit/s
+        ser.write(b"O\r") #Open canusb
+        print(ser.read(5))
+        print("CANUSB activated")
     else:
-        t+=1
-        print("No message")
-    #close port when finished
-ser.write(b"C\r") #Close CANUSB
-ser.close()
+        print( "Port did not open")
+    return ser
+#Sort messages
+def sort_messages(ser,t):
+        print("No. " + str(t))
+        x = ser.read(22)
+        print(x)
+        y = list(x)
+        if x != "":
+            joined = "0x" + y[1] + y[2] + y[3]
+            node_id = int(joined,0)
+            data = ""
+            for j in range(5,21,1):
+                data += y[j]
+            #data = data.decode("hex")
+            print("Node " + str(node_id) + " message:" + "\n" + data + "\n")
+        else:
+            print("No message")
+
+#close port when finished
+def close_canusb(ser):
+    ser.write(b"C\r") #Close CANUSB
+
+    ser.close()
+
+
+def check_input(ser,file):
+    input = file.read(8)
+    print("Message to send:")
+    if input == "":
+        print("No message sent")
+    else:
+        file.seek(file.tell()+2)
+        a=""
+        for x in input:
+            a = a + ("0"+((hex(ord(x)))[2:]))[-2:]
+        print(a.decode("hex"))
+        ser.write(b"t1118"+a+"\r")
+        print("t1118"+a+"\r")
+        x=ser.read(2)
+        if x == "z\r":
+            print("Message Sent\n")
+        else:
+            print("Message not sent!\n")
+    print(file.tell())
+
+
+
+ser = open_ports("COM7",115200,1)
+file = open("C:\Users\Ed\Desktop\CAN_Input.txt","r")
+for t in range(0,10000,1):
+    sort_messages(ser,t)
+    check_input(ser,file)
+close_canusb(ser)
+
