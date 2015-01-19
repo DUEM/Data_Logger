f706454c015c0beaf4b9bdbe2e0ceacd5a1a1eca
usmaanjaved <usm44njaved@gmail.com>
2015-01-19 20:50:07 +0000

    Added a bunch of stuff

--- GUI.py
+++ GUI.py
@@ -2,23 +2,34 @@ from tkinter import *
 import socket, sys, threading
 from queue import Queue
 import numpy as np
+import pylab
 import matplotlib.pyplot as plot
+from matplotlib.pylab import subplots,close
+from matplotlib import cm
 import time,sys,os
 from datetime import datetime
 
 # allows communication between threads
 q = Queue()	 # talk to communication thread
 q.join()
-q2 = Queue() # talk to live plot battery thread
+q2 = Queue()    # talk to live plot battery thread
 q2.join()
-q3 = Queue() # talk to saving csv battery thread
+q3 = Queue()    # talk to saving csv battery thread
 q3.join()
+q4 = Queue()    # talk to live plot speed thread
+q4.join()
+q5 = Queue()    # talk to  saving csv speed thread
+q5.join()
+q6 = Queue()    # talk to  live plot temperature thread
+q6.join()
+q7 = Queue()    # talk to  saving csv temperature thread
+q7.join()
 ###################### Sending/Recieving data bit ###########################
 def comunication():
         ## CLIENT
         t = True
         host = "localhost"
-        port = 51423
+        port = 51432
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             sock.connect((host,port)) # sock.connec_ex((host,port)) !!!
@@ -44,12 +55,20 @@ def comunication():
                         print(msg2)
                         #OutShell.insert(END,msg2+'\n')
                         #OutShell.see(END)
-                        if "_PLOT_BAT_" in msg2:
+                        if "_COM_LIVE_PLOT_BAT_" in msg2:
                             q2.put(msg2)
-                        elif  "_SAVE_BAT_" in msg2:
+                        elif  "_COM_SAVE_BAT_" in msg2:
                                 q3.put(msg2)
+                        elif "_COM_LIVE_PLOT_SPEED_" in msg2:
+                                q4.put(msg2)
+                        elif "_COM_SAVE_SPEED_" in msg2:
+                                q5.put(msg2)
+                        elif "_COM_LIVE_PLOT_TEMP_" in msg2:
+                                q6.put(msg2)
+                        elif "_COM_SAVE_TEMP_" in msg2:
+                                q7.put(msg2) 
                         else:
-                                OutShell.insert(END,msg2+'\n')
+                                OutShell.insert(END,'\n'+'>'*80+'\n'+msg2+'\n'+'<'*80+'\n\n')
                                 OutShell.see(END)
             except:
                 print("Failed to recieve the message")
@@ -63,11 +82,13 @@ comm.start()
 ################################# Live Plot #################################
 def bat_plot(Qout):
         message = Qout.get() # get command to send forr more data
-        print(message)
         Qout.task_done()
-        print(message)
-        plot.ion()
-        fig = plot.figure()
+        variable = Qout.get() # get variable name for filenames
+        Qout.task_done()
+        #plot.ion()
+        pylab.ion()
+        #fig = plot.figure()
+        fig = pylab.figure()
         fig.show()
         stop_message = "_STOP_"
         while True:
@@ -75,17 +96,22 @@ def bat_plot(Qout):
                 if data == stop_message:
                         # save figure then close
                         Time = datetime.utcnow().strftime("%d-%m-%Y")
-                        filename = "SavedPlots/bat - " + Time + ".png"
+                        filename = "SavedPlots/"+variable+" - " + Time + ".png"
                         fig.savefig(filename)
                         OutShell.insert(END,'Data saved to file: ' + filename +'\n')
                         OutShell.see(END)
+                        close(fig)
                         break
-                data = data.replace("_PLOT_BAT_","")
+                data = data.replace(message,"")
                 try:
-                        plot.scatter(time.clock(),float(data))
-                        plot.draw()
+                        x = time.clock()
+                        y = float(data)
+                        #time.sleep(0.5)
+                        pylab.scatter(x,y)
+                        pylab.draw()
+                        #time.sleep(0.5)
                 except:
-                        a = 1
+                        break
                 Qout.task_done()
                 q.put(message)
 				
@@ -104,7 +130,7 @@ def CSVsave(qout):
                 data = qout.get()
                 if data == stop_message:
                         break
-                data = data.replace("_SAVE_BAT_","")
+                data = data.replace(message,"")
                 try:
                         f=open(file,'a')
                         f.write(str(time.clock())+','+str(data)+'\n')
@@ -119,17 +145,34 @@ def CSVsave(qout):
 ################################ GUI STUFF THINGS ABOUT TO GET CRAY #########################################
 
 def send_can(): # function that sends can message
-        message = CanInput.get()
+        Inmessage = CanInput.get()
+        if intType == 0:
+                message = int(Inmessage, 16)
+        else:
+                message = int(Inmessage,10)
+        OutShell.insert(END,'Sending Can Message: '+message+'\n') 
+        OutShell.see(END)
         message = "_SEND_CAN_MESSAGE_" + message #add pefix so sever knows what sort of data it is
         q.put(message)
         
         return 1
 
 def send_command(): # funcion that sends commands
-	message = LoggerInput.get()
-	message = "COM" + message
-	q.put(message)
-	return 1
+        message = LoggerInput.get()
+        OutShell.insert(END,'Sending Server Command: '+message+'\n') 
+        OutShell.see(END)
+        message = "COM" + message
+        q.put(message)
+        return 1
+
+def SendCommands(command, but1, but2): # funcion that sends commands
+        message = command
+        OutShell.insert(END,'Sending Server Command: '+message+'\n') 
+        OutShell.see(END)
+        q.put(message)
+        toggle(but1)
+        toggle(but2)
+        return 1
 ################################################################
 
 # general structure for LivePlot and SaveDataCSV
@@ -151,6 +194,7 @@ def LivePlot(variable, command, qout, enab_but, disab_but):
         #placing command into empty que so it is the first message to be read by the thread
         message = command
         qout.put(message)
+        qout.put(variable) 
         # start thread
         BatPlot = threading.Thread(target = lambda: bat_plot(qout))
         BatPlot.daemon = True
@@ -163,6 +207,7 @@ def LivePlot(variable, command, qout, enab_but, disab_but):
         # toggle buttons
         toggle(enab_but)
         toggle(disab_but)
+        
         return 1
 
 def SaveDataCSV(variable, command, qout, enab_but, disab_but): 
@@ -177,7 +222,7 @@ def SaveDataCSV(variable, command, qout, enab_but, disab_but):
         f = open(filename,'a')
         f.close()
         #printing to output terminal
-        OutShell.insert(END,'Saving data to file: '+filename+'\n') 
+        OutShell.insert(END,'Saving' + variable + 'data to file: '+filename+'\n') 
         OutShell.see(END)
         #start thread
         CSV = threading.Thread(target = lambda: CSVsave(qout))
@@ -211,11 +256,10 @@ def toggle(but): # toggles buttons states
                 but.config(state='disabled')
 
 def Buttons(): # creates buttons
-
         #Send CAN Message
         but1=Button(Inputs, text='Send Can Message', command=send_can, width=30)                          
         but1.grid(row=0,column=1)
-
+        
         #Send Command to Datalogger
         but2=Button(Inputs, text='Send Command to Datalogger', command=send_command, width=30)                            
         but2.grid(row=1,column=1)
@@ -241,6 +285,56 @@ def Buttons(): # creates buttons
 
         but5.config( command= lambda: SaveDataCSV('Battery', "_COM_SAVE_BAT_", q3, but6, but5))
         but6.config( command= lambda: SendStopMessage(q3, but5, but6))
+
+        #Live Plot Speed
+        but7=Button(Inputs, text='Live Plot Speed',  width=30, state='normal')  
+        but7.grid(row=4,column=0)
+        #Stop Live Plot Speed
+        but8=Button(Inputs, text='Stop Live Plot Speed',  width=30, state='disabled')  
+        but8.grid(row=4,column=1)
+
+        but7.config( command= lambda: LivePlot("Speed","_COM_LIVE_PLOT_SPEED_",q4,but7,but8))
+        but8.config( command= lambda: SendStopMessage(q4,but7,but8))
+
+        #Save Speed Data To CSV
+        but9=Button(Inputs, text='Save Speed Data To CSV',  width=30, state='normal')  
+        but9.grid(row=5,column=0)
+        #Stop Saving Speed Data To CSV
+        but10=Button(Inputs, text='Stop Saving Speed Data To CSV',  width=30, state='disabled')  
+        but10.grid(row=5,column=1)
+
+        but9.config( command= lambda: SaveDataCSV('Speed', "_COM_SAVE_SPEED_", q5, but9, but10))
+        but10.config( command= lambda: SendStopMessage(q5, but9, but10))        
+
+        #Monitor All CAN Activity
+        but11=Button(Inputs, text='Monitor All CAN Activity',  width=30, state='normal')  
+        but11.grid(row=6,column=0)
+        #Stop Monitoring All CAN Activity
+        but12=Button(Inputs, text='Stop Monitoring All CAN Activity',  width=30, state='disabled')  
+        but12.grid(row=6,column=1)
+        
+        but11.config( command= lambda: SendCommands("_MONITOR_CAN_BUS_", but11, but12))
+        but12.config( command= lambda: SendCommands("_STOP_MONITOR_CAN_BUS_", but11, but12))
+
+        #Live Plot Temperature
+        but13=Button(Inputs, text='Live Plot Temperature',  width=30, state='normal')  
+        but13.grid(row=7,column=0)
+        #Stop Live Plot Temperature
+        but14=Button(Inputs, text='Stop Live Plot Temperature',  width=30, state='disabled')  
+        but14.grid(row=7,column=1)
+
+        but13.config( command= lambda: LivePlot("Temperature","_COM_LIVE_PLOT_TEMP_",q6,but13,but14))
+        but14.config( command= lambda: SendStopMessage(q6,but13,but14))
+
+        #Save Temperature Data To CSV
+        but15=Button(Inputs, text='Save Temperature Data To CSV',  width=30, state='normal')  
+        but15.grid(row=8,column=0)
+        #Stop Saving Temperature Data To CSV
+        but16=Button(Inputs, text='Stop Saving Temperature Data To CSV',  width=30, state='disabled')  
+        but16.grid(row=8,column=1)
+
+        but15.config( command= lambda: SaveDataCSV('Temperature', "_COM_SAVE_TEMP_", q7, but15, but16))
+        but16.config( command= lambda: SendStopMessage(q7, but15, but16))
         
         return 1
 
@@ -251,6 +345,8 @@ root.title('USM system (Ultimate Solar-car Management system)') #adds title to w
 Inputs=LabelFrame(root,text='Command Area',height=200,width=200, padx=5, pady=5)      #creates a frame where the input widgets will go
 Inputs.grid(row=0)
 
+inpType = 0
+
 ################ TEXT FIELDS ###################	
 #HAVE TO BE GLOBAL :(
 CanInput = Entry(Inputs, width=30)
@@ -259,6 +355,14 @@ CanInput.focus_set()
 
 LoggerInput = Entry(Inputs, width=30)
 LoggerInput.grid(row = 1, column = 0)
+
+#Radio Button
+hexSelect = Radiobutton(Inputs, text="Hex", variable=inpType, value=0)
+hexSelect.grid(row = 0, column = 3)
+#Radio Button
+AsciiSelect = Radiobutton(Inputs, text="Ascii", variable=inpType, value=1)
+AsciiSelect.grid(row = 1, column = 3)
+
 ##################################################
 Buttons()
 
@@ -268,8 +372,4 @@ Outputs.grid(row=0, column=1)
 OutShell = Text(Outputs)
 OutShell.grid(row=0,column=0)
 
-
-
 root.mainloop()
-
-
