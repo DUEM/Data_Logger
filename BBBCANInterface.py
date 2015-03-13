c43127c4f135d26a6d5d5023bf6d15069708e920
edwardsainsbury <edwardsainsbury@me.com>
2015-03-13 18:22:19 +0000

    Update BBBCANInterface.py

--- BBBCANInterface.py
+++ BBBCANInterface.py
@@ -71,12 +71,11 @@ database="test"
 password="runrun93"
 #connect = connect(username,password,database)
 #cursor = cursor(connect)
-#sync = open("CAN_TimeSync.canusb","r")
-#line = sync.readline()
-#print(line)
-#file = open(line,"r")
+sync = open("CAN_TimeSync.canusb","r")
+line = sync.readline()
+file = open(line,"r")
 while 1:
-    #check_input(file)
+    check_input(file)
     cf, addr = s.recvfrom(can_frame_size)
     can_id, can_dlc, data = dissect_can_frame(cf)
     print('Received: can_id=%x '% can_id)
