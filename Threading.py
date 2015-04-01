13a092c39264b20665e5867ccd882e05aa2af995
edwardsainsbury <edwardsainsbury@me.com>
2015-04-01 22:02:08 +0100

    Update Threading.py

--- Threading.py
+++ Threading.py
@@ -256,6 +256,7 @@ def SendCanMessage(can_frame_fmt, can_id,cansock):
 		print("message is")
 		print(message)
 		canmessage = struct.pack(can_frame_fmt, can_id, can_dlc, message)
+		canmessage = struct.pack(can_frame_fmt, 0x400, 2, b"\x00\x00")
 		cansock.send(canmessage)
 		q2.task_done() #Marks the message as sent so it can move on to the next
 		print("message sent")
