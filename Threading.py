4a7b430049cafb1128ad5f1d20ce583b2ff5e902
edwardsainsbury <edwardsainsbury@me.com>
2015-04-05 01:01:34 +0100

    Update Threading.py

--- Threading.py
+++ Threading.py
@@ -16,138 +16,138 @@ def threadFunc(conn, addr):
 	while 1:
         #connect = SQL_interface.connect(SQL_interface.username,SQL_interface.password,SQL_interface.database)
         #cursor = SQL_interface.cursor(connect)
-	try: #getting message
-		message1 = conn.recv(2048)
-		if not len(message1):
-			print("Disconnected with " + addr[0] + ":" + str(addr[1]))
+		try: #getting message
+			message1 = conn.recv(2048)
+			if not len(message1):
+				print("Disconnected with " + addr[0] + ":" + str(addr[1]))
+				conn.close()
+				break
+			if type(message1) == bytes:
+				message1 = message1.decode("utf-8")
+				print("Received: " + str(message1))
+			else:
+				print("Received: " + str(message1))
+		except:
+			print("Failed to recieve the message")
 			conn.close()
 			break
-		if type(message1) == bytes:
-			message1 = message1.decode("utf-8")
-			print("Received: " + str(message1))
-		else:
-			print("Received: " + str(message1))
-	except:
-		print("Failed to recieve the message")
-		conn.close()
-		break
-	if message1 == "_db_":
-		if addr[0] == "127.0.0.1":
-			global db_clients_INFO
-			info = get_info(db_clients_INFO)
+		if message1 == "_db_":
+			if addr[0] == "127.0.0.1":
+				global db_clients_INFO
+				info = get_info(db_clients_INFO)
+				msg1 = (str(info)).encode("utf-8")
+			else:
+				info = "You don\'t have permission to access this database\n"
+				msg1 = (str(info)).encode("utf-8")
+		elif message1 == "_info_":
+			info = "1) _db_ for users info database\n"
+			info += "2) _mv_ for movies database\n"
 			msg1 = (str(info)).encode("utf-8")
-		else:
-			info = "You don\'t have permission to access this database\n"
+	#############################################################################################################################
+	#******************************************** Sensor Data Request **********************************************************#
+	#############################################################################################################################
+		elif message1 == "Speed_data":
+	            #get speed data here (query sql database or get it live of the CAN)
+	            #Query SQL
+	            
+	            ##########
+			info = "Speed_data: "
+			info += str(speed_data)
 			msg1 = (str(info)).encode("utf-8")
-	elif message1 == "_info_":
-		info = "1) _db_ for users info database\n"
-		info += "2) _mv_ for movies database\n"
-		msg1 = (str(info)).encode("utf-8")
-#############################################################################################################################
-#******************************************** Sensor Data Request **********************************************************#
-#############################################################################################################################
-	elif message1 == "Speed_data":
-            #get speed data here (query sql database or get it live of the CAN)
-            #Query SQL
-            
-            ##########
-		info = "Speed_data: "
-		info += str(speed_data)
-		msg1 = (str(info)).encode("utf-8")
-	elif message1 == "Temp_data_all":
-            #get temp data from all sources here (query sql database or get it live of the CAN)
-            #query SQL
-            ##########
-		info = "Temp data" + str(source_id) + ": " #get the id of the node which sent the data
-		info += str(temp_data)
-		msg1 = (str(info)).encode("utf-8")
-	elif message1 == "Bat_SOC":
-            #get SOC data (query sql database or get it live of the CAN) 
-            #going to query SQL
-            ###################
-		info = "SOC: " 
-		info += str(SOC)
-		msg1 = (str(info)).encode("utf-8")
-############################################################################################################################
-#******************************************************** Recieving Commands **********************************************#
-############################################################################################################################
-	elif "_SEND_CAN_MESSAGE_" in message1:
-            # checks if want to send a can message 
-            #send message to other programme here
-		message1 = message1.replace("_SEND_CAN_MESSAGE_","")
-		print("sending Can Message: " + message1)
-		print(message1)
-		if "," in message1:
-			message = message1.split(",")
-			if (message[0]!="")and(message[1]!="")and(message[1].isalpha() != 1)and(message[0].isalpha() != 1)and(len(message[1])/2) == int(message[0]):				q2.put(message)
-				print("message sent")
-				msg1 =("Message sent"+message1).encode("utf-8")
+		elif message1 == "Temp_data_all":
+	            #get temp data from all sources here (query sql database or get it live of the CAN)
+	            #query SQL
+	            ##########
+			info = "Temp data" + str(source_id) + ": " #get the id of the node which sent the data
+			info += str(temp_data)
+			msg1 = (str(info)).encode("utf-8")
+		elif message1 == "Bat_SOC":
+	            #get SOC data (query sql database or get it live of the CAN) 
+	            #going to query SQL
+	            ###################
+			info = "SOC: " 
+			info += str(SOC)
+			msg1 = (str(info)).encode("utf-8")
+	############################################################################################################################
+	#******************************************************** Recieving Commands **********************************************#
+	############################################################################################################################
+		elif "_SEND_CAN_MESSAGE_" in message1:
+	            # checks if want to send a can message 
+	            #send message to other programme here
+			message1 = message1.replace("_SEND_CAN_MESSAGE_","")
+			print("sending Can Message: " + message1)
+			print(message1)
+			if "," in message1:
+				message = message1.split(",")
+				if (message[0]!="")and(message[1]!="")and(message[1].isalpha() != 1)and(message[0].isalpha() != 1)and(len(message[1])/2) == int(message[0]):				q2.put(message)
+					print("message sent")
+					msg1 =("Message sent"+message1).encode("utf-8")
+				else:
+					print("message error")
+					msg1 =("Message Error"+message1).encode("utf-8")
 			else:
 				print("message error")
-				msg1 =("Message Error"+message1).encode("utf-8")
-		else:
-			print("message error")
-	  		msg1 =("Message Error"+message1).encode("utf-8")
-            ##################################### 
-	elif "_SET_MESSAGE_FREQUENCY_" in message1:
-            # checks if want to change the rate a messages are sent at 
-		msg_freq = message1[-1] # set the frequency of messages
-	elif "_SET_SAVE_FREQUENCY_" in message1:
-            # checks if want to change how often to save on the loggers local storage
- 		save_freq = message1[-1] #set message save frequency
-	elif "_STOP_RECORDING_CAN_" in message1:
-            #send command to other programme here
-		print("stop")
-            ####################################
-	elif "_MONITOR_CAN_BUS_" == message1:
-            # get live can bus activity and send to client
-            # probs create a thread to wait for activity and send to the client
-            #message1 = "Displaying Live CAN Activity"
-            #last_message_time="2015-02-04 16:20:26.558000"
-		last_message_time = "401"
-            #a_long_time="2020-02-04 16:20:26.558000"
-		a_long_time="499"
-		latest_message = SQL_interface.query(cursor,connect,"Node ID",last_message_time,a_long_time)
-		for (Time, Node_ID, Data) in latest_message:
-			message1 = "_MONITOR_CAN_BUS_"+"{1}, {0} {2}".format(Time, Node_ID, Data)
-			print(message1)
+		  		msg1 =("Message Error"+message1).encode("utf-8")
+	            ##################################### 
+		elif "_SET_MESSAGE_FREQUENCY_" in message1:
+	            # checks if want to change the rate a messages are sent at 
+			msg_freq = message1[-1] # set the frequency of messages
+		elif "_SET_SAVE_FREQUENCY_" in message1:
+	            # checks if want to change how often to save on the loggers local storage
+	 		save_freq = message1[-1] #set message save frequency
+		elif "_STOP_RECORDING_CAN_" in message1:
+	            #send command to other programme here
+			print("stop")
+	            ####################################
+		elif "_MONITOR_CAN_BUS_" == message1:
+	            # get live can bus activity and send to client
+	            # probs create a thread to wait for activity and send to the client
+	            #message1 = "Displaying Live CAN Activity"
+	            #last_message_time="2015-02-04 16:20:26.558000"
+			last_message_time = "401"
+	            #a_long_time="2020-02-04 16:20:26.558000"
+			a_long_time="499"
+			latest_message = SQL_interface.query(cursor,connect,"Node ID",last_message_time,a_long_time)
+			for (Time, Node_ID, Data) in latest_message:
+				message1 = "_MONITOR_CAN_BUS_"+"{1}, {0} {2}".format(Time, Node_ID, Data)
+				print(message1)
+				msg1 = (str(message1)).encode("utf-8")
+		elif "_STOP_MONITOR_CAN_BUS_" == message1:
+	            #stops sending live CAN data
+			message1 = "No Longer Displaying Live CAN Activity"
 			msg1 = (str(message1)).encode("utf-8")
-	elif "_STOP_MONITOR_CAN_BUS_" == message1:
-            #stops sending live CAN data
-		message1 = "No Longer Displaying Live CAN Activity"
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_LIVE_PLOT_BAT_" in message1:
-		message1 = "_COM_LIVE_PLOT_BAT_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_SAVE_BAT_" in message1:
-		message1 = "_COM_SAVE_BAT_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_LIVE_PLOT_SPEED_" in message1:
-		message1 = "_COM_LIVE_PLOT_SPEED_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_SAVE_SPEED_" in message1:
-		message1 = "_COM_SAVE_SPEED_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_LIVE_PLOT_TEMP_" in message1:
-		message1 = "_COM_LIVE_PLOT_TEMP_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-	elif "_COM_SAVE_TEMP_" in message1:
-		message1 = "_COM_SAVE_TEMP_" + str(random.randrange(1,10+1))
-		msg1 = (str(message1)).encode("utf-8")
-#############################################################################################################################
-#******************************************************** Errors ***********************************************************#
-#############################################################################################################################
-	else:
-		message1 = "Unknown command \nType: _info_ for a list of commands"
-		msg1 = (str(message1)).encode("utf-8")
-#############################################################################################################################
-#**************************************************** Sending Message ******************************************************#
-#############################################################################################################################
-	try:
-		conn.sendall(msg1)
-	except:
-		print("Failed to send the message")
-
+		elif "_COM_LIVE_PLOT_BAT_" in message1:
+			message1 = "_COM_LIVE_PLOT_BAT_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+		elif "_COM_SAVE_BAT_" in message1:
+			message1 = "_COM_SAVE_BAT_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+		elif "_COM_LIVE_PLOT_SPEED_" in message1:
+			message1 = "_COM_LIVE_PLOT_SPEED_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+		elif "_COM_SAVE_SPEED_" in message1:
+			message1 = "_COM_SAVE_SPEED_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+		elif "_COM_LIVE_PLOT_TEMP_" in message1:
+			message1 = "_COM_LIVE_PLOT_TEMP_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+		elif "_COM_SAVE_TEMP_" in message1:
+			message1 = "_COM_SAVE_TEMP_" + str(random.randrange(1,10+1))
+			msg1 = (str(message1)).encode("utf-8")
+	#############################################################################################################################
+	#******************************************************** Errors ***********************************************************#
+	#############################################################################################################################
+		else:
+			message1 = "Unknown command \nType: _info_ for a list of commands"
+			msg1 = (str(message1)).encode("utf-8")
+	#############################################################################################################################
+	#**************************************************** Sending Message ******************************************************#
+	#############################################################################################################################
+		try:
+			conn.sendall(msg1)
+		except:
+			print("Failed to send the message")
+	
 
 def con_info(addr):
 	host,port = addr
