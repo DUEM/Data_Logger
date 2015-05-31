e52d2bd7531209a389896d0664409e8b4ffbb83b
edwardsainsbury <edwardsainsbury@me.com>
2015-05-31 20:08:36 +0100

    Update Threading.py

--- Threading.py
+++ Threading.py
@@ -244,6 +244,7 @@ def SendCanMessage(can_frame_fmt, can_id,cansock):
 	while 1:
 		message = q2.get()
 		print(message)
+		print(struct.pack(can_frame_fmt, can_id, int(message[0]), (bytes.fromhex(message[1])).ljust(8,b"\x00")))
 		cansock.send(struct.pack(can_frame_fmt, can_id, int(message[0]), (bytes.fromhex(message[1])).ljust(8,b"\x00")))
 		q2.task_done() 
 db_clients_INFO = []
