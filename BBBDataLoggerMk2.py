1d5b955adaf199dcb461232aedc7a54d20345dec
EndingCredits <awwoof@hotmail.com>
2015-09-10 15:21:20 +0100

    Update BBBDataLoggerMk2.py

--- BBBDataLoggerMk2.py
+++ BBBDataLoggerMk2.py
@@ -6,7 +6,7 @@ can_interface_type = 'socketcan_ctypes'
 bus = can.interface.Bus(can_interface, can_interface_type)
 
 while 1:
-  """ message = bus.recv() """
+  # message = bus.recv()
   
   msg = can.Message(arbitration_id=0xc0ffee, data=[2, 0, 0, 1, 3, 1, 4, 1], extended_id=False)
   bus.send(msg)
