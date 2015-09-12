da38ea5fe0bd7f53f6e4e124b467dca729fb11b3
EndingCredits <awwoof@hotmail.com>
2015-09-12 14:31:22 +0100

    Update BBBDataLoggerMk2.py

--- BBBDataLoggerMk2.py
+++ BBBDataLoggerMk2.py
@@ -1,5 +1,6 @@
 import can
 import mysql.connector as sql
+import datetime
 
 #################################### SQL FUNCTIONS WE WILL NEED #########################################################
 def connect(username,password,database):
@@ -8,9 +9,14 @@ def connect(username,password,database):
 def cursor(connect):
     cursor = connect.cursor()
     return cursor
-def add_message(data,cursor,connect):
+def add_message(msg_id, data,cursor,connect):
+    time1 = datetime.datetime.now()
     ###################################### Creating the Argument #######################
-    add_message = "INSERT INTO can ( `msg_data` ) VALUES ('"
+    add_message = "INSERT INTO can ( 'msg_time' 'msg_id', `msg_data` ) VALUES ('"
+    add_message += str(time1)
+    add_message += "', '"
+    add_message += msg_id
+    add_message += "', '"
     add_message += data
     add_message += "')"
     print(add_message)
@@ -24,17 +30,21 @@ database="test"
 password="dusc2015"
 connect = connect(username,password,database)
 cursor = cursor(connect)
-
+# CREATE TABLE can( msg_no BIGINT AUTO_INCREMENT PRIMARY KEY, msg_time DATETIME, msg_id INT, msg_data VARBINARY(8));
 
 can_interface = 'can0'
 can_interface_type = 'socketcan_ctypes'
 
 bus = can.interface.Bus(can_interface, can_interface_type)
 
+msg = can.Message(arbitration_id=0x520, data=[2, 0, 0, 1, 3, 1, 4, 1], extended_id=False)
+add_message(msg.arbiration_id, msg.data, cursor, connect)
 
+"""
 while 1:
   # add_message(str(bus.recv()), cursor, connect)
   
   msg = can.Message(arbitration_id=0xc0ffee, data=[2, 0, 0, 1, 3, 1, 4, 1], extended_id=False)
   print(msg.data);
   # bus.send(msg)
+"""
