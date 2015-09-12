4d0c075c94267a30337acaa9cbdf785ba92c9bae
EndingCredits <awwoof@hotmail.com>
2015-09-12 14:42:45 +0100

    Update BBBDataLoggerMk2.py

--- BBBDataLoggerMk2.py
+++ BBBDataLoggerMk2.py
@@ -10,12 +10,14 @@ def cursor(connect):
     cursor = connect.cursor()
     return cursor
 def add_message(msg_id, data,cursor,connect):
-    time1 = datetime.datetime.now()
+    #time1 = datetime.datetime.now()
     ###################################### Creating the Argument #######################
-    add_message = "INSERT INTO can ( 'msg_time', 'msg_id') VALUES ("
-    add_message += str(time1)
-    add_message += ", "
-    add_message += str(msg_id)
+    add_message = "INSERT INTO can ( 'msg_id', 'msg_data' ) VALUES ('"
+    #add_message += str(time1)
+    #add_message += ", "
+    add_message += int(msg_id)
+    add_message += "', "
+    add_message += str(msg_data)
     add_message += ")"
     print(add_message)
     cursor.execute(add_message)
