43b33f2f86c96e40d207cb5fd19b7fc64818ed03
EndingCredits <awwoof@hotmail.com>
2015-09-12 14:34:27 +0100

    Update BBBDataLoggerMk2.py

--- BBBDataLoggerMk2.py
+++ BBBDataLoggerMk2.py
@@ -15,9 +15,9 @@ def add_message(msg_id, data,cursor,connect):
     add_message = "INSERT INTO can ( 'msg_time' 'msg_id', `msg_data` ) VALUES ('"
     add_message += str(time1)
     add_message += "', '"
-    add_message += msg_id
+    add_message += str(msg_id)
     add_message += "', '"
-    add_message += data
+    add_message += str(data)
     add_message += "')"
     print(add_message)
     cursor.execute(add_message)
