1b6255632c208456ac750b6ae665300b0c398f9f
EndingCredits <awwoof@hotmail.com>
2015-09-27 18:37:42 +0100

    Update send.php

--- send.php
+++ send.php
@@ -10,7 +10,9 @@
 <form >
 ID:  <input type="text" name="msg_id" value="400" size ="4" />
 
-Data:  <input type="text" name="msg_data" size="16" />
+Data:  <input type="text" name="msg_data" size="20" />
+
+<input type="submit" name="submit" value="Submit" />
 </form>
 
 
